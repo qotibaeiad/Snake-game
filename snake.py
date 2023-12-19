61168182eb5b5a60654e62fcf0a1207114a3e850
user <user@users-MacBook-Pro.local>
2023-12-19 02:39:32 +0200

    fix

--- snake.py
+++ snake.py
@@ -1,9 +1,15 @@
 import pygame
 import sys
+import random
+import math
 
+def calculate_distance(x1, y1, x2, y2):
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return distance
 # Initialize Pygame
 pygame.init()
-
+max_distance= calculate_distance(20,20,25,25)
+print("the distance is max:",max_distance)
 # Set up display
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
@@ -25,18 +31,23 @@ head = None
 pos_x = 200
 pos_y = 200
 rect_color = (0, 128, 255)  # RGB color tuple
+random_rec_color = (255 , 0,0)
 rect_position = (pos_x, pos_y)  # (x, y) position of the top-left corner
 rect_size = (20, 20)
+tmp = True
+x = random.randint(0, 780)
+y = random.randint(0,580)
+dis = calculate_distance(pos_x,pos_y,x,y)
 
 # Main game loop
 running = True
-speed = 0.08  # Adjusted the speed value
+speed = 0.20  # Adjusted the speed value
 
 # Flags to indicate movement direction
 moving_up = False
 moving_down = False
 moving_left = False
-moving_right = False
+moving_right = True
 #head.add_rectangle(screen, rect_color, pygame.Rect(rect_position, rect_size))
 while running:
     for event in pygame.event.get():
@@ -67,24 +78,28 @@ while running:
     
        
     # Update position based on movement flags
-    if moving_up and pos_y > 0:
+    if moving_up and pos_y > 0 and dis >= max_distance:
         pos_y -= speed
-    if moving_down and pos_y + rect_size[1] < height:
+    if moving_down and pos_y + rect_size[1] < height and dis >= max_distance:
         pos_y += speed
-    if moving_left and pos_x > 0:
+    if moving_left and pos_x > 0 and dis >= max_distance:
         pos_x -= speed
-    if moving_right and pos_x + rect_size[0] < width:
+    if moving_right and pos_x + rect_size[0] < width and dis >= max_distance:
         pos_x += speed
 
     # Paint the screen
     screen.fill((255, 255, 255))  # Fill the screen with white color
 
-    # Update rectangle position
+    # draw rand rectangle
     rect_position = (pos_x, pos_y)
-
-    # Draw the rectangle
     pygame.draw.rect(screen, rect_color, pygame.Rect(rect_position, rect_size))
-
+    dis = calculate_distance(pos_x,pos_y,x,y)
+    while 0 < dis < max_distance:
+        x = random.randint(0, 780)
+        y = random.randint(0,580)
+        dis = calculate_distance(pos_x,pos_y,x,y)
+    random_rec = (x, y)
+    pygame.draw.rect(screen, random_rec_color, pygame.Rect(random_rec, rect_size))
     # Update the display
     pygame.display.flip()
 
