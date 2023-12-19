3f56d4d2a4a0c5f85c1cd2eb75e16b3999fbc7e5
qotibamhamed <qotibaeiad98@gmail.com>
2023-12-19 03:50:03 +0200

    fix logic there issue

--- snake.py
+++ snake.py
@@ -3,103 +3,201 @@ import sys
 import random
 import math
 
+
+class Node:
+    def __init__(self, x, y, color):
+        self.x_position = x
+        self.y_position = y
+        self.color = color
+        self.next = None
+
+    def prepend(self, x, y, color):
+        new_node = Node(x, y, color)
+        new_node.next = self.head
+        self.head = new_node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def prepend(self, x, y, color):
+        new_node = Node(x, y, color)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete(self, x, y, color):
+        if self.is_empty():
+            return
+
+        if self.head.x_position == x and self.head.y_position == y:
+            self.head = self.head.next
+            return
+
+        current = self.head
+        while current.next and (current.next.x_position != x or current.next.y_position != y or current.next.color != color):
+            current = current.next
+
+        if current.next:
+            current.next = current.next.next
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.x_position, end=" -> ")
+            current = current.next
+        print("None")
+
+
 def calculate_distance(x1, y1, x2, y2):
-    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-    return distance
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+
+def create_node(x, y, color):
+    return {"rect": pygame.Rect(x, y, 20, 20), "color": color, "next": None}
+
+
+def draw_rectangle(screen, rect, color):
+    pygame.draw.rect(screen, color, rect)
+
+
 # Initialize Pygame
 pygame.init()
-max_distance= calculate_distance(20,20,25,25)
-print("the distance is max:",max_distance)
+
 # Set up display
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Snake game")
+score_variable=0
+#Text
+pygame.font.init()
+font = pygame.font.Font(None, 36)
+# Create a text surface
+score_text = font.render("Score: {}".format(score_variable), True, (255, 1, 1))  # Adjust the color as needed
 
+# Get the rect object from the text surface
+text_rect = score_text.get_rect()
 
-class Node:
-    def __init__(self, x, y, color):
-        self.rect = pygame.Rect(x, y, 20, 20)
-        self.color = color
-        self.next = None
+# Set the position of the text (adjust as needed)
+text_rect.topleft = (10, 10)
 
-def add_rectangle(x, y, color):
-    new_node = Node(x, y, color)
-    new_node.next = head
-    return new_node
-
-head = None
-pos_x = 200
-pos_y = 200
-rect_color = (0, 128, 255)  # RGB color tuple
-random_rec_color = (255 , 0,0)
-rect_position = (pos_x, pos_y)  # (x, y) position of the top-left corner
+# Blit the text surface onto the screen
+screen.blit(score_text, text_rect)
+# Initialize variables
+linked_list = LinkedList()
+pos_x, pos_y = 200, 200
+rect_color = (0, 128, 255)
+linked_list.prepend(200, 200, rect_color)
+random_rec_color = (255, 0, 0)
 rect_size = (20, 20)
-tmp = True
-x = random.randint(0, 780)
-y = random.randint(0,580)
-dis = calculate_distance(pos_x,pos_y,x,y)
+max_distance = calculate_distance(20, 20, 40, 40)
+
+# Initialize x and y
+x, y = random.randint(0, 780), random.randint(0, 580)
+dis = calculate_distance(pos_x, pos_y, x, y)
 
 # Main game loop
 running = True
-speed = 0.20  # Adjusted the speed value
+speed = 0.12
 
 # Flags to indicate movement direction
 moving_up = False
 moving_down = False
 moving_left = False
 moving_right = True
-#head.add_rectangle(screen, rect_color, pygame.Rect(rect_position, rect_size))
+
+# Event loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        # Check for key presses
         elif event.type == pygame.KEYDOWN:
+            # Check for key presses
+            moving_up, moving_down, moving_left, moving_right = False, False, False, False
             if event.key == pygame.K_w:
                 moving_up = True
-                moving_down = False
-                moving_left = False
-                moving_right = False
             elif event.key == pygame.K_s:
-                moving_up = False
                 moving_down = True
-                moving_left = False
-                moving_right = False
             elif event.key == pygame.K_a:
-                moving_up = False
-                moving_down = False
                 moving_left = True
-                moving_right = False
             elif event.key == pygame.K_d:
-                moving_up = False
-                moving_down = False
-                moving_left = False
                 moving_right = True
-    
-       
+
     # Update position based on movement flags
-    if moving_up and pos_y > 0 and dis >= max_distance:
-        pos_y -= speed
-    if moving_down and pos_y + rect_size[1] < height and dis >= max_distance:
-        pos_y += speed
-    if moving_left and pos_x > 0 and dis >= max_distance:
-        pos_x -= speed
-    if moving_right and pos_x + rect_size[0] < width and dis >= max_distance:
-        pos_x += speed
+    if dis >= max_distance:
+        if moving_up: #and pos_y > 0:
+            #Wfor rec in linked_list:
+             #   rec.x_position-=speed
+            current = linked_list.head
+            while current:
+                if current.next:
+                    current.y_position = current.next.y_position
+                else:
+                    current.y_position -= speed
+                current = current.next
+            pos_y -= speed
+        elif moving_down:# and pos_y + rect_size[1] < height:
+            current = linked_list.head
+            while current:
+                if current.next:
+                    current.y_position = current.next.y_position
+                else:
+                    current.y_position += speed
+                current = current.next
+            pos_y += speed
+        elif moving_left:# and pos_x > 0:
+            current = linked_list.head
+            while current:
+                if current.next:
+                    current.x_position = current.next.x_position
+                else:
+                    current.x_position -= speed
+                current = current.next
+            pos_x -= speed
+        elif moving_right:# and pos_x + rect_size[0] < width:
+            current = linked_list.head
+            while current:
+                if current.next:
+                    current.x_position = current.next.x_position
+                else:
+                    current.x_position += speed
+                current = current.next
+            pos_x += speed
 
     # Paint the screen
-    screen.fill((255, 255, 255))  # Fill the screen with white color
+    screen.fill((255, 255, 255))
 
-    # draw rand rectangle
+    # Draw rectangles
     rect_position = (pos_x, pos_y)
-    pygame.draw.rect(screen, rect_color, pygame.Rect(rect_position, rect_size))
-    dis = calculate_distance(pos_x,pos_y,x,y)
+    #draw_rectangle(screen, pygame.Rect(rect_position, rect_size), rect_color)
+
+    # Draw random rectangle
+    dis = calculate_distance(pos_x, pos_y, x, y)
+
     while 0 < dis < max_distance:
-        x = random.randint(0, 780)
-        y = random.randint(0,580)
-        dis = calculate_distance(pos_x,pos_y,x,y)
+        linked_list.prepend(linked_list.head.x_position,linked_list.head.y_position,rect_color)
+        score_variable+=1
+        x, y = random.randint(0, 780), random.randint(0, 580)
+        dis = calculate_distance(pos_x, pos_y, x, y)
     random_rec = (x, y)
-    pygame.draw.rect(screen, random_rec_color, pygame.Rect(random_rec, rect_size))
+    draw_rectangle(screen, pygame.Rect(random_rec, rect_size), random_rec_color)
+
+    current = linked_list.head
+    while current:
+        draw_rectangle(screen,pygame.Rect((current.x_position,current.y_position),rect_size),rect_color)
+       # print(current.x_position, end=" -> ")
+        current = current.next
+    #print("None")
+
+
+
+    score_text = font.render("Score: {}".format(score_variable), True, (255, 1, 1))
+    screen.blit(score_text, text_rect)
+
+
     # Update the display
     pygame.display.flip()
 
